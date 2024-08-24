bb1094fa66e445d8d73f03469c658f777e0eed5d
Vijay Sinha <vijaysinha60@gmail.com>
2024-08-24 02:22:00 +0000

    All buttons have there own color profile that can be used for setting background of surrounding.

--- color_Project/script.js
+++ color_Project/script.js
@@ -1,1 +1,28 @@
+// setting red color here
+const container = document.querySelector(".container")
+const red = document.getElementById("red")
+red.addEventListener('click', (event) => {
+    container.style.background = "red";
+})
 
+// setting green color here
+const green = document.getElementById("green")
+green.addEventListener('click', (event) => {
+    container.style.background = "green";
+})
+
+// setting blue color here
+const blue = document.getElementById("blue")
+blue.addEventListener('click', (event) => {
+    container.style.background = "blue";
+})
+// setting orange color here
+const orange = document.getElementById("orange")
+orange.addEventListener('click', (event) => {
+    container.style.background = "orange";
+})
+// setting lime color here
+const lime = document.getElementById("lime")
+lime.addEventListener('click', (event) => {
+    container.style.background = "lime";
+})
